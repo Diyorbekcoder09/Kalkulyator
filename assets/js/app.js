30033bffb865d52347755a508b40156fa92d69d3
diyorbek2009 <diyorbekcoder09@gmail.com>
2024-11-05 13:18:12 +0500

    kalkuyator

---
+++ assets/js/app.js
@@ -0,0 +1,99 @@
+btn1.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value = eval(input.value ** 2);
+});
+
+btn2.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value = Math.sqrt(input.value);
+});
+
+btn3.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value = Math.round(input.value);
+});
+
+btn4.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value = "";
+});
+
+btn5.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value += "1";
+});
+
+btn6.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value += "2";
+});
+
+btn7.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value += "3";
+});
+
+btn8.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value += "*";
+});
+
+btn9.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value += "4";
+});
+
+btn10.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value += "5";
+});
+
+btn11.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value += "6";
+});
+
+btn12.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value += "/";
+});
+
+btn13.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value += "7";
+});
+
+btn14.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value += "8";
+});
+
+btn15.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value += "9";
+});
+
+btn16.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value += "-";
+});
+
+btn17.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value += "0";
+});
+
+btn18.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value += ".";
+});
+
+btn19.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value = eval(input.value);
+});
+
+btn20.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("input").value += "+";
+});
